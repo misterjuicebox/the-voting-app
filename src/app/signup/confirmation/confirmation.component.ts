545990303fef875467c5c35871b02632e07844ee
misterjuicebox <bootstrapjuicebox@gmail.com>
2019-03-05 07:43:26 -0800

    changes to confirmation

--- src/app/signup/confirmation/confirmation.component.ts
+++ src/app/signup/confirmation/confirmation.component.ts
@@ -2,7 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import { AuthService } from "../../auth.service";
 import {Observable} from "rxjs";
 import 'rxjs/add/observable/forkJoin';
-import { Router} from "@angular/router";
+import { Router } from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
@@ -13,14 +14,27 @@ export class ConfirmationComponent implements OnInit {
   @Input() user: any = {};
 
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      debugger;
+      this.confirmSignUp(params);
+    })
+  }
+
+    confirmSignUp(params: any) {
+        this.authService.confirmSignUp(params.username, params.code).subscribe(result => {
+          console.log(result);
+          debugger;
+    });
   }
 
   submit(user: any) {
     const email = user.email, code = user.code, password = user.password;
-    let authRequests = [];
+    const authRequests = [];
 
     authRequests.push(this.authService.confirmSignUp(email, code));
     authRequests.push(this.authService.signIn(email, password));
@@ -30,8 +44,7 @@ export class ConfirmationComponent implements OnInit {
       if (result[0] === 'SUCCESS' && this.authService.isAuthenticated()) {
         this.router.navigate(['/']);
       }
-      debugger;
-    })
+    });
     // this.authService.confirmSignUp(email, code).subscribe(result => {
     //     debugger;
     //     this.authService.signIn(email, password).subscribe(result => {
--- src/app/signup/signup-routing.module.ts
+++ src/app/signup/signup-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 import {SignupComponent} from "./signup.component";
+import {ConfirmationComponent} from './confirmation/confirmation.component';
 
 const routes: Routes = [
-    {path: '', component: SignupComponent}
+    {path: '', component: SignupComponent},
+    {path: 'confirmation', component: ConfirmationComponent}
 ];
 
 @NgModule({
